3bd2f98d0f5aae2b4cc2fbb27c37c732b2a8891b
zdl <3218992152@qq.com>
2022-12-05 19:22:07 +0800

    improve structure

--- acapp/urls.py
+++ acapp/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from game.urls.index import index
 
 urlpatterns = [
-    # path('', include('game.urls')),
+    path('', include('game.urls.index')),
     path('admin/', admin.site.urls),
 ]
